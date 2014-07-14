92f762a61cd1edd1b2e7fdf662fda5bc0a4e1483
Jesse <jesse_108@163.com>
2014-07-14 08:03:08 +0800

    build code

---
+++ conf/db.conf.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * 数据库配置
+ */
+$dbConfig = array();
+////主数据库
+$dbConfig['rw'] = array(
+	'host' => 'localhost',
+	'user' => 'root',
+	'password' => '123456',
+	'name' => 'teemo',
+);
+
+
+//只读数据库
+$dbConfig['ro'] = array(
+	'host' => 'localhost',
+	'user' => 'root',	
+	'password' => '123456',
+	'name' => 'teemo',	
+);
+
+
+$config['db'] = $dbConfig;
---
+++ temp/compile/b70d448d935c83fbfccbfd225f58df7212a35ad0.file.footer.html.php
@@ -0,0 +1,23 @@
+<?php /* Smarty version Smarty-3.1.14, created on 2014-06-13 16:28:55
+         compiled from "D:\develop\code\www\php_base\template\footer.html" */ ?>
+<?php /*%%SmartyHeaderCode:270539ab6477b10e1-83001147%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    'b70d448d935c83fbfccbfd225f58df7212a35ad0' => 
+    array (
+      0 => 'D:\\develop\\code\\www\\php_base\\template\\footer.html',
+      1 => 1387525346,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '270539ab6477b10e1-83001147',
+  'function' => 
+  array (
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.14',
+  'unifunc' => 'content_539ab6477b4330_34101048',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_539ab6477b4330_34101048')) {function content_539ab6477b4330_34101048($_smarty_tpl) {?></body>
+</html><?php }} ?>
