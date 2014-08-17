ea6a47ec0c103323d19cbe7a1f4e65a1514b8822
zhaojian@mac_pro <jesse_108@163.com>
2014-08-18 07:11:21 +0800

    location list

--- webroot/location/list.php
+++ webroot/location/list.php
@@ -8,16 +8,15 @@ $countryID = $_GET['country_id'];
 
 $allLocations = Lib_Location::GetAllLocation(0,true,true);
 
-
-if($contientID){
+if($countryID){
+	$cities = $allLocations[$contientID]['sub'][$countryID]['sub'];
+} else if($contientID){
 	$cities = array();
 	foreach ($allLocations[$contientID]['sub'] as $country){
 		if($country['sub']){
 			$cities = array_merge($cities,$country['sub']);
 		}
 	}
-} else if($countryID){
-	$cities = $allLocations[$contientID]['sub'][$countryID]['sub'];
 }
 
 Template::Show();
